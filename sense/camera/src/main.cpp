e877902b2856651b9bc77f31b13bc021c4857baa
Nicolas Pope <nwpope@gmail.com>
2015-06-09 17:02:29 +0200

    Working webcam input?

--- sense/camera/src/main.cpp
+++ sense/camera/src/main.cpp
@@ -6,9 +6,327 @@
 
 #include <iostream>
 
+#include <fcntl.h>              /* low-level i/o */
+#include <unistd.h>
+#include <errno.h>
+#include <malloc.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#include <asm/types.h>          /* for videodev2.h */
+
+#include <linux/videodev2.h>
+
 using dharc::Sense;
 using dharc::Node;
 using std::cout;
+
+int fd = -1;
+
+#define CLEAR(x) memset (&(x), 0, sizeof (x))
+
+constexpr size_t width = 320;
+constexpr size_t height = 240;
+constexpr size_t size() { return width * height; }
+
+struct buffer
+{
+    void *start;
+    size_t length;
+};
+
+buffer *buffers;
+
+float fbuffer[size()];
+const char *dev_name = "/dev/video0";
+
+static void errno_exit(const char *s)
+{
+    fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
+
+    exit(EXIT_FAILURE);
+}
+
+static int xioctl(int fd, int request, void *arg)
+{
+    int r;
+
+    do
+    {
+        r = ioctl(fd, request, arg);
+    }
+    while (-1 == r && EINTR == errno);
+
+    return r;
+}
+
+int read_frame(void)
+{
+	struct v4l2_buffer buf;
+
+    CLEAR(buf);
+
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_USERPTR;
+
+    if (-1 == xioctl(fd, VIDIOC_DQBUF, &buf))
+    {
+        switch (errno)
+        {
+        case EAGAIN:
+            return 0;
+
+        case EIO:
+            /* Could ignore EIO, see spec. */
+
+            /* fall through */
+
+        default:
+            errno_exit("VIDIOC_DQBUF");
+        }
+    }
+
+    //for (i = 0; i < n_buffers; ++i)
+     //buf.m.userptr == (unsigned long)buffers[0].start
+     //buf.length == buffers[0].length)
+            //break;
+
+    //assert(i < n_buffers);
+
+    // process_image((void *)buf.m.userptr);
+
+    if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+        errno_exit("VIDIOC_QBUF");
+
+	return 1;
+}
+
+
+static void init_userp(unsigned int buffer_size)
+{
+    struct v4l2_requestbuffers req;
+    unsigned int page_size;
+
+    page_size = getpagesize();
+    buffer_size = (buffer_size + page_size - 1) & ~(page_size - 1);
+
+    CLEAR(req);
+
+    req.count = 1;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_USERPTR;
+
+    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
+    {
+        if (EINVAL == errno)
+        {
+            fprintf(stderr, "%s does not support "
+                    "user pointer i/o\n", dev_name);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            errno_exit("VIDIOC_REQBUFS");
+        }
+    }
+
+    buffers = (buffer*)calloc(1, sizeof(*buffers));
+
+    if (!buffers)
+    {
+        fprintf(stderr, "Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //for (n_buffers = 0; n_buffers < 4; ++n_buffers)
+    //{
+        buffers[0].length = buffer_size;
+        buffers[0].start = memalign( /* boundary */ page_size,
+                                            buffer_size);
+
+        /*if (!buffers[n_buffers].start)
+        {
+            fprintf(stderr, "Out of memory\n");
+            exit(EXIT_FAILURE);
+        }*/
+    //}
+}
+
+
+void init_device(void)
+{
+    struct v4l2_capability cap;
+    struct v4l2_cropcap cropcap;
+    struct v4l2_crop crop;
+    struct v4l2_format fmt;
+    // unsigned int min;
+
+    if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &cap))
+    {
+        if (EINVAL == errno)
+        {
+            fprintf(stderr, "%s is no V4L2 device\n", dev_name);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            errno_exit("VIDIOC_QUERYCAP");
+        }
+    }
+
+    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE))
+    {
+        fprintf(stderr, "%s is no video capture device\n", dev_name);
+        exit(EXIT_FAILURE);
+    }
+
+
+    if (!(cap.capabilities & V4L2_CAP_STREAMING))
+    {
+        fprintf(stderr, "%s does not support streaming i/o\n", dev_name);
+        exit(EXIT_FAILURE);
+    }
+
+
+    /* Select video input, video standard and tune here. */
+
+
+    CLEAR(cropcap);
+
+    cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    if (0 == xioctl(fd, VIDIOC_CROPCAP, &cropcap))
+    {
+        crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        crop.c = cropcap.defrect;   /* reset to default */
+
+        if (-1 == xioctl(fd, VIDIOC_S_CROP, &crop))
+        {
+            switch (errno)
+            {
+            case EINVAL:
+                /* Cropping not supported. */
+                break;
+            default:
+                /* Errors ignored. */
+                break;
+            }
+        }
+    }
+    else
+    {
+        /* Errors ignored. */
+    }
+
+
+    CLEAR(fmt);
+
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmt.fmt.pix.width = width;
+    fmt.fmt.pix.height = height;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+    fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+
+    if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
+        errno_exit("VIDIOC_S_FMT");
+
+    /* Note VIDIOC_S_FMT may change width and height. */
+
+    /* Buggy driver paranoia. */
+    /*min = fmt.fmt.pix.width * 2;
+    if (fmt.fmt.pix.bytesperline < min)
+        fmt.fmt.pix.bytesperline = min;
+    min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
+    if (fmt.fmt.pix.sizeimage < min)
+        fmt.fmt.pix.sizeimage = min;*/
+
+    //if (fmt.fmt.pix.width != 320)
+    //    WIDTH = fmt.fmt.pix.width;
+
+   // if (fmt.fmt.pix.height != HEIGHT)
+   //     HEIGHT = fmt.fmt.pix.height;
+
+	init_userp(fmt.fmt.pix.sizeimage);
+}
+void stop_capturing(void)
+{
+    enum v4l2_buf_type type;
+
+    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    if (-1 == xioctl(fd, VIDIOC_STREAMOFF, &type))
+        errno_exit("VIDIOC_STREAMOFF");
+}
+
+void start_capturing(void)
+{
+    enum v4l2_buf_type type;
+
+    struct v4l2_buffer buf;
+
+    CLEAR(buf);
+
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_USERPTR;
+    buf.index = 0;
+    buf.m.userptr = (unsigned long)buffers[0].start;
+    buf.length = buffers[0].length;
+
+    if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+        errno_exit("VIDIOC_QBUF");
+
+    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    if (-1 == xioctl(fd, VIDIOC_STREAMON, &type))
+        errno_exit("VIDIOC_STREAMON");
+}
+
+
+void uninit_device(void)
+{
+    free(buffers[0].start);
+    free(buffers);
+}
+
+
+void close_device()
+{
+    if (-1 == close(fd))
+        errno_exit("close");
+
+    fd = -1;
+}
+
+
+void open_device()
+{
+    struct stat st;
+
+    if (-1 == stat(dev_name, &st))
+    {
+        fprintf(stderr, "Cannot identify '%s': %d, %s\n",
+                dev_name, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (!S_ISCHR(st.st_mode))
+    {
+        fprintf(stderr, "%s is no device\n", dev_name);
+        exit(EXIT_FAILURE);
+    }
+
+    fd = open(dev_name, O_RDWR /* required */  | O_NONBLOCK, 0);
+
+    if (-1 == fd)
+    {
+        fprintf(stderr, "Cannot open '%s': %d, %s\n",
+                dev_name, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(int argc, char *argv[]) {
 	Sense sense("localhost", 7878);
@@ -16,16 +334,52 @@ int main(int argc, char *argv[]) {
 	Node cam_f, cam_l;
 	sense.makeHarcs(320 * 240, cam_f, cam_l);
 
+	open_device();
+	init_device();
+	start_capturing();
+
 	vector<float> data;
 	data.resize(320 * 240);
 
-	for (auto i = 0U; i < data.size(); ++i) {
-		data[i] = 1.0f;
-	}
+	while (true) {
+		fd_set fds;
+        struct timeval tv;
+        int r;
 
-	for (auto k = 0; k < 1000; ++k) {
+        FD_ZERO(&fds);
+        FD_SET(fd, &fds);
+
+        /* Timeout. */
+        tv.tv_sec = 2;
+        tv.tv_usec = 0;
+
+        r = select(fd + 1, &fds, NULL, NULL, &tv);
+
+        if (-1 == r)
+        {
+            if (EINTR == errno)
+                continue;
+
+            errno_exit("select");
+        }
+
+        if (0 == r)
+        {
+            fprintf(stderr, "select timeout\n");
+            exit(EXIT_FAILURE);
+        }
+
+		read_frame();
+		for (auto i = 0U; i < data.size(); ++i) {
+			unsigned char y = *((char*)buffers[0].start + (2*i));
+			data[i] = 1.0f / (255.0f - static_cast<float>(y));
+		}
 		sense.activate(cam_f, cam_l, data);
 	}
+
+	stop_capturing();
+	uninit_device();
+	close_device();
 
 	return 0;
 }
